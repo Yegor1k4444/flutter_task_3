c48bd63a80853538f8fb1fa053fbdabda160eb49
Yegor <160131141+Yegor1k4444@users.noreply.github.com>
2025-12-11 06:03:44 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,55 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Color myButtonColor = Colors.red;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Введіть число (напр. 25, 50, 80)',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      int number = int.tryParse(text) ?? 0;
+                      if (number < 30) {
+                        myButtonColor = Colors.red;
+                      } else if (number > 70) {
+                        myButtonColor = Colors.green;
+                      } else {
+                        myButtonColor = Colors.yellow;
+                      }
+                    });
+                  },
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: myButtonColor),
+                onPressed: () {},
+                child: const Text(
+                  'Кнопка',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
